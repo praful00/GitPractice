771c5b8461831b42719ec7af41482fbb167e830a
Praful <prafulshinde00777@gmail.com>
2023-10-12 15:25:10 +0530

    commit by sujay

--- src/connection.java
+++ src/connection.java
@@ -30,6 +30,7 @@ public class connection {
 			System.out.println(rs.getString("loaction"));
 			System.out.println(rs.getString("age"));
 			System.out.println("succesfull");
+			System.out.println("Database testing");
 		}
 	}
 
